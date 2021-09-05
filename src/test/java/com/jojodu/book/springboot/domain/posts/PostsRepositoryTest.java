f0a53942a4486589c32995a36cda81a41674101c
joon <joonk0409@gmail.com>
2021-09-05 22:14:51 +0900

    Entity, Service, Controller 생성

--- src/test/java/com/jojodu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojodu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.jojodu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.Application;
 import com.jojoldu.book.springboot.domain.Posts;
 import com.jojoldu.book.springboot.domain.PostsRepository;
 import org.junit.After;
@@ -11,8 +12,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class PostsRepositoryTest {
 
     @Autowired
@@ -40,7 +43,8 @@ public class PostsRepositoryTest {
 
         //then
         Posts posts = postsList.get(0);
-
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
     }
 
 }
